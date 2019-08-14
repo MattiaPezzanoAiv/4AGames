9ae7ccaaf07f413ff77a9bf20ff6906a07cd4188
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2019-08-14 18:58:02 +0200

    removed unused files

--- Challenge_4AGames/Defines.h
+++ Challenge_4AGames/Defines.h
@@ -2,7 +2,7 @@
 #define WINDOW_W 800
 #define WINDOW_H 800
 
-#define BULLET_SPEED 200.0f
+#define BULLET_SPEED 300.0f
 #define BULLET_RADIUS 5.0f
 #define BULLET_COLOR sf::Color::Green
 #define WALL_COLOR sf::Color::Cyan
